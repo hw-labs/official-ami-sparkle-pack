09f1d83f448cacb5207de816c8c7160809bddb27
Michael F. Weinberg <michael@hw-ops.com>
2015-12-16 21:38:42 -0800

    Ready to play with!

---
+++ lib/sparkleformation/registry/official_amis.rb
@@ -0,0 +1,29 @@
+SfnRegistry.register(:official_amis) do |_name, _config = {}|
+
+  type = _config.fetch(:type, 'instance_store').to_sym
+  virt = _config.fetch(:virt, 'hvm').to_sym
+  os_versions = _config.fetch(:os_versions, ['ubuntu_14', 'ubuntu_12', 'amazon_2015_09'])
+
+  ami_hash = {}
+
+  os_versions.each do |os|
+
+    ami_hash[os] = registry!("#{os}_amis".to_sym)
+    #   :precise => registry!(:ubuntu_12_amis),
+    #   :amazon_2015_09 => registry!(:amazon_linux_2015_09_amis)
+    # }
+  end
+  
+  ami_hash.each do |release, info|
+    info[:amis].each do |region, ids|
+      mappings do
+        official_amis do
+          camel_keys_set!(:auto_disable)
+          set!(region) do
+            set!(info[:version], ids[virt][type])
+          end
+        end
+      end
+    end
+  end
+end
--- lib/sparkleformation/test.rb
+++ lib/sparkleformation/test.rb
@@ -1,4 +1,4 @@
 SparkleFormation.new(:test) do
 
-  registry!(:ubuntu_official_amis, :test, :type => 'instance_store', :virt => 'hvm')
+  registry!(:official_amis, :test)
 end
